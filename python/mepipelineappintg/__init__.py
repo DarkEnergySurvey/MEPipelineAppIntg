9a37c2b3f424caff138f0c1ce59e4740a6f9560e
Felipe Menanteau <felipe@illinois.edu>
2016-06-22 16:15:11 +0000

    Getting ready for new tag

--- python/mepipelineappintg/__init__.py
+++ python/mepipelineappintg/__init__.py
@@ -1,5 +1,5 @@
-_author__  = "Robert Gruendl"
-__version__ = '1.0.0'
+_author__  = "Robert Gruendl, Michelle Gower, Felipe Menanteau"
+__version__ = '1.0.2'
 version = __version__
 from os import environ
 proddir = environ['MEPIPELINEAPPINTG_DIR']
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='mepipelineappintg',
-      version ='1.0.0',
+      version ='1.0.2',
       license = "GPL",
       description = "A set python utils apps for the DES multiepoch pipeline",
       author = "Michelle Gower",
