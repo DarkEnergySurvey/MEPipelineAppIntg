3ac7196a16cdb50aa5a4c89237d4a0efe923ac26
Felipe Menanteau <felipe@illinois.edu>
2017-01-13 18:58:33 +0000

    Tagging trunk as 1.1.4

--- python/mepipelineappintg/__init__.py
+++ python/mepipelineappintg/__init__.py
@@ -1,5 +1,5 @@
 _author__  = "Robert Gruendl, Michelle Gower, Felipe Menanteau"
-__version__ = '1.1.2'
+__version__ = '1.1.4'
 version = __version__
 from os import environ
 proddir = environ['MEPIPELINEAPPINTG_DIR']
