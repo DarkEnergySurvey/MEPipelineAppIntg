2554236f8543ec26ce3fe7ac99b2de129823dfef
Felipe Menanteau <felipe@illinois.edu>
2017-02-06 15:57:52 +0000

    Tagging trunk as 1.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='mepipelineappintg',
-      version ='1.1.4',
+      version ='1.1.5',
       license = "GPL",
       description = "A set python utils apps for the DES multiepoch pipeline",
       author = "Michelle Gower",
