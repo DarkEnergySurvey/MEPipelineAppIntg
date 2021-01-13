5a1376e75ddaee2d4a5e93069123438a59ef0ca2
Felipe Menanteau <felipe@illinois.edu>
2021-01-13 11:13:28 -0600

    Updates to for taging version 3.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='mepipelineappintg',
-      version = '3.3.1',
+      version = '3.3.2',
       license = "GPL",
       description = "A set python utils apps for the DES multiepoch pipeline",
       author = "Michelle Gower",
