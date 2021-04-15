2dbc91c1d4fccd02624cc0a4975fda2ac73685a2
Felipe Menanteau <felipe@illinois.edu>
2021-04-15 15:17:29 -0500

    Update version to 3.3.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='mepipelineappintg',
-      version = '3.3.6',
+      version = '3.3.7',
       license = "GPL",
       description = "A set python utils apps for the DES multiepoch pipeline",
       author = "Michelle Gower",
