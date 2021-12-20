8769b22f2f31f7293c905a7cc9bec0d252678d1c
Robert Gruendl <gruendl@illinois.edu>
2021-12-20 11:22:08 -0600

    Update setup.py to 3.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='mepipelineappintg',
-      version = '3.3.9',
+      version = '3.4.1',
       license = "GPL",
       description = "A set python utils apps for the DES multiepoch pipeline",
       author = "Michelle Gower",
