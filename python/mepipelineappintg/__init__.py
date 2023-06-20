a57be325ce40f3020570a2dbd229dd8dab80a99d
Robert Gruendl <gruendl@illinois.edu>
2023-06-20 12:33:48 -0500

    Tag 3.4.4

--- python/mepipelineappintg/__init__.py
+++ python/mepipelineappintg/__init__.py
@@ -1,6 +1,6 @@
 from os import environ
 
 _author__ = "Robert Gruendl, Felipe Menanteau, Michelle Gower"
-__version__ = '3.4.3'
+__version__ = '3.4.4'
 version = __version__
 proddir = environ['MEPIPELINEAPPINTG_DIR']
